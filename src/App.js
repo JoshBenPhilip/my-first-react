c7a6aa0f81dc3d79f26c5931fbd6a7a9ecc2da47
JoshBenPhilip <joshben4c@gmail.com>
2022-05-12 16:52:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [count, setCount]= useState(0)
+  const [firstName, setFirstName] = useState("Josh")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn)
   return (
-    <div className="App">
+    <div className="whatever i want">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome {isLoggedIn ? firstName: 'Guest'}.</h1>
+      <button onClick={loginLogout}>
+        {isLoggedIn ? 'Logout': 'Login'}
+      </button>
+        
+
+        <h2>You clicked the button {count} times.</h2>
+        <button onClick={() => setCount(count + 1)}>+</button>&nbsp;
+        <button onClick={() => setCount(0)}>reset</button>&nbsp;
+        <button className='btn-red' onClick={() => setCount(count ? count - 1: 0)}>-</button>
+        
+        
+        
+      <section>
+      </section>
       </header>
     </div>
   );
 }
 
 export default App;
+ 
